d033d372dc7d31e77ecd1cff37bf7b31e4eed9b6
Gleb333-3 <glebefremenko23@gmail.com>
2021-05-02 23:28:29 +0300

    commit

---
+++ Assets/light.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _Lig;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Lig.Rotate(Vector3.up * Time.deltaTime * 5);
+    }
+}
