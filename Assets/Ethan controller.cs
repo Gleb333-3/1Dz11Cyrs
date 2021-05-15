b494c15afc1c951bf47c74044b93e8fb5c04dbeb
Gleb333-3 <glebefremenko23@gmail.com>
2021-05-15 23:51:42 +0300

    commit

---
+++ Assets/Ethan controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private GameObject _target;
+
+    [SerializeField]
+    private float _weightLeftHand;
+
+    protected void OnAnimatorIK(int layerIndex)
+    {
+        _animator.SetIKPosition(goal: AvatarIKGoal.LeftHand, _target.transform.position);
+        _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, _weightLeftHand);
+    }
+}
---
+++ Assets/Ethancontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ethancontroller : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private GameObject _target;
+
+    [SerializeField]
+    private GameObject _flashlight;
+
+    [SerializeField]
+    private float _weightLeftHand;
+
+    protected void OnAnimatorIK(int layerIndex)
+    {
+        _weightLeftHand = (Vector3.Dot(transform.forward, _target.transform.position - transform.position) > 0) ? 1 : 0;
+
+        _animator.SetIKPosition(goal: AvatarIKGoal.LeftHand, _target.transform.position);
+        _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, _weightLeftHand);
+
+        _animator.SetIKPosition(AvatarIKGoal.LeftHand, _flashlight.transform.position);
+        _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+
+    }
+}
