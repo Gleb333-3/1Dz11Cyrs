a3e412ef0d5a89c5c579007616228fe878df8500
Gleb333-3 <glebefremenko23@gmail.com>
2021-05-19 23:37:46 +0300

    commit

---
+++ Assets/Postprocessefffectcontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using System.Linq;
+
+public class Postprocessefffectcontroller : MonoBehaviour
+{
+    private PostProcessVolume _activeVolume;
+
+    protected void Awake()
+    {
+        var postProcessValumes = new List<PostProcessVolume>();
+        PostProcessManager.instance.GetActiveVolumes(layer: GetComponent<PostProcessLayer>(), postProcessValumes);
+        _activeVolume = postProcessValumes.First();
+    }
+
+    protected void Start()
+    {
+        Vignette vignette;
+        if(!_activeVolume.profile.TryGetSettings(out vignette))
+        {
+            vignette = _activeVolume.profile.AddSettings<Vignette>();
+        }
+        vignette.color.value = Color.red;
+        vignette.intensity.value = 0.0f;
+    }
+
+  
+}
